8635398add103d1e112952005d23cf34921cb5d8
Toby <52330522+nhtoby311@users.noreply.github.com>
2023-08-26 12:39:06 +0200

    flip card on click css

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,20 @@
 import styles from "./Card.module.css";
 
-export default function Card() {
+type Props = {
+  children: React.ReactNode;
+  active?: boolean;
+  onClickCB?: () => void;
+};
+
+export default function Card({ children, active, onClickCB }: Props) {
   return (
-    <div className={styles.container}>
-      <div className={styles["back-pattern"]}></div>
+    <div className={styles.container} onClick={onClickCB}>
+      <div className={`${styles.content}`} data-active={active ? true : false}>
+        <div className={styles.back}>
+          <div className={styles["back-pattern"]} />
+        </div>
+        <div className={styles.front}>{children}</div>
+      </div>
     </div>
   );
 }
--- src/pages/Loading/Loading.tsx
+++ src/pages/Loading/Loading.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import Button from "../../components/Button/Button";
 import Card from "../../components/Card/Card";
 import styles from "./Loading.module.css";
 
 export default function Loading() {
+  const [activeCard, setActiveCard] = useState(0);
+
   return (
     <>
       <div></div>
       <div className={styles.container}>
         <div className={`g-container ${styles["cards-cont"]}`}>
-          <Card />
-          <Card />
-          <Card />
+          <Card
+            onClickCB={() => {
+              setActiveCard(0);
+            }}
+            active={activeCard === 0}
+          >
+            1
+          </Card>
+          <Card onClickCB={() => setActiveCard(1)} active={activeCard === 1}>
+            2
+          </Card>
+          <Card onClickCB={() => setActiveCard(2)} active={activeCard === 2}>
+            1
+          </Card>
         </div>
         <div className={`g-container ${styles["descrip-cont"]}`}>
           <p>
@@ -19,7 +33,7 @@ export default function Loading() {
           </p>
         </div>
         <div className={`g-container ${styles["percentage-cont"]}`}>
-          <span>88%</span>
+          {/* <span>88%</span> */}
           <Button title="Single Player" />
           <Button title="Multi Player" />
         </div>
