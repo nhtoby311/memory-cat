f7235021471445d3e7fd8b274ac7df4d63ea5cb8
Toby <52330522+nhtoby311@users.noreply.github.com>
2023-08-30 05:55:17 +0200

    add tilt card fx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,5 @@
 import styles from "./Card.module.css";
+import Tilt from "react-parallax-tilt";
 
 type Props = {
   active?: boolean;
@@ -8,15 +9,27 @@ type Props = {
 
 export default function Card({ active, onClickCB, url }: Props) {
   return (
-    <div className={styles.container} onClick={onClickCB}>
-      <div className={`${styles.content}`} data-active={active ? true : false}>
-        <div className={styles.back}>
-          <div className={styles["back-pattern"]} />
-        </div>
-        <div className={styles.front}>
-          <img className={styles.image} src={url} alt="cat picture" />
+    <Tilt
+      // glareEnable={true}
+      // glareMaxOpacity={0.1}
+      // glareColor="#ffffff"
+      // glarePosition="bottom"
+      // glareBorderRadius="10px"
+      tiltReverse={true}
+    >
+      <div className={styles.container} onClick={onClickCB}>
+        <div
+          className={`${styles.content}`}
+          data-active={active ? true : false}
+        >
+          <div className={styles.back}>
+            <div className={styles["back-pattern"]} />
+          </div>
+          <div className={styles.front}>
+            <img className={styles.image} src={url} alt="cat picture" />
+          </div>
         </div>
       </div>
-    </div>
+    </Tilt>
   );
 }
